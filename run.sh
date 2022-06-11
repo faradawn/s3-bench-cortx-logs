ac8ccbb1fac846afd8c7f8b8c141106f0aa3b6fe
faradawn <faradawny@gmail.com>
2022-06-11 08:17:25 -0500

    8 nodes 2 pods 4g

--- run.sh
+++ run.sh
@@ -1,25 +1,27 @@
-start=$(date +%s)
-
 export IP=192.168.84.128
-export PORT=31477
+export PORT=31336
 
-for nreqs in 1000; do
-    for objsize in 1024; do
-        for nclients in 1 5 10; do
+for nreqs in 100; do
+    for objsize in 512 256 2048 16384; do
+        for nclients in 1 5 10 20 40 80 120 160; do
+            start=$(date +%s)
             nsamples=$(( ($nclients * $nreqs) ))
-            path="logs/k8cluster=4/npods=2/podsize=4g/nreqs=${nreqs}/objsize=${objsize}k/nclients=${nclients}"
-            echo "${path}/se.log"
+            path="logs/k8cluster=8/npods=2/podsize=4g/nreqs=${nreqs}/objsize=${objsize}k/nclients=${nclients}"
             mkdir -p $path
-            ./s3bench -accessKey gregoryaccesskey -accessSecret gregorysecretkey -bucket loadgen -endpoint http://$IP:$PORT -objectNamePrefix=loadgen -region us-east-1 \
+            echo "performing ${path}"
+            ./s3bench -accessKey gregoryaccesskey -accessSecret gregorysecretkey -endpoint http://$IP:$PORT -objectNamePrefix=loadgen -region us-east-1 \
+            -bucket "bucket-${nreqs}-${objsize}-${nclients}" \
             -objectSize "${objsize}Kb" \
             -numClients $nclients \
             -numSamples $nsamples \
             -o "${path}/s3-bench-log.txt"
+            
+
+            end=$(date +%s)
+            runtime=$(( (end-start)/60 ))
+            echo "runtime is $runtime minutes"
         done
     done
 done
 
-rm -f *2020-6*
-end=$(date +%s)
-runtime=$(( (end-start)/60 ))
-echo "runtime is $runtime minutes"
+rm -f *2022-6*
