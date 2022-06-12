9d6ea83cf6f7b7caa7ee2c96457c0494dcdc1c5c
faradawn <faradawny@gmail.com>
2022-06-12 09:02:00 -0500

    test numpods 2

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 export IP=192.168.84.128
-export PORT=32261
+export PORT=30494
 
 for nreqs in 100; do
     for objsize in 1024; do
         for nclients in 80; do
             start=$(date +%s)
             nsamples=$(( ($nclients * $nreqs) ))
-            path="logs/k8cluster=8/npods=4/podsize=64g/nreqs=${nreqs}/objsize=${objsize}k/nclients=${nclients}"
+            path="logs/k8cluster=8/npods=2/podsize=64g/nreqs=${nreqs}/objsize=${objsize}k/nclients=${nclients}"
             mkdir -p $path
             echo "performing ${path}"
             ./s3bench -accessKey gregoryaccesskey -accessSecret gregorysecretkey -endpoint http://$IP:$PORT -objectNamePrefix=loadgen -region us-east-1 \
