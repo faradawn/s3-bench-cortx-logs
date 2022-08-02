be80f8f0a48b6f701aae3ee800db8b32c99ba188
faradawn <73060648+faradawn@users.noreply.github.com>
2022-08-02 10:30:58 -0500

    update to test 4 8 16 MB

--- 07-30_s3logs_minio_k8s_10k_req/bench_minio_perf.sh
+++ 07-30_s3logs_minio_k8s_10k_req/bench_minio_perf.sh
@@ -1,23 +1,10 @@
-## New method: using s3 speedtest (perf)
+# ./minio server /mnt/data
+# 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384
 
 shopt -s expand_aliases
 source /etc/bashrc
 
-for i in 4 8 16 32 64 128 256 512 1024 2048; do
+for i in 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384; do
     TOTAL_SIZE=10000*$i
     mc support perf object myminio/ --size ${i}KiB --blocksize ${i}KiB --concurrent 1 --filesize $TOTAL_SIZE
 done
-
-
-
-## Old method: create files and upload 
-
-# for i in 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384; do
-#     ./create_file $i 1 -o
-# done
-
-# source /etc/bashrc
-#/home/cc/minio-bench/mc mb myminio/bucket1
-# upload and download a file
-# ./mc cp 16KB myminio/bucket1/
-# ./mc cp myminio/bucket1/16KB .
